3d74e2ad5399fb729cd7271ea888ac248e2eba2c
klen1980 <nick300880@gmail.com>
2023-07-24 22:36:17 +0300

    start add3ing s3e3nd emails

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import EmailPostForm
 
 def post_list(request):
     posts = Post.published.all()
@@ -16,4 +17,17 @@ def post_detail(request, year, month, day,post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form})
 # Create your views here.
