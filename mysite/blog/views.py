7f0255198ea936d636a3d3b5d3b9f5de1bb36afc
Nik <nik3008@gmail.com>
2023-08-05 23:01:26 +0300

    add comments to posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
+from django.views.decorators.http import require_POST
 
 
 def post_share(request, post_id):
@@ -60,7 +61,27 @@ def post_detail(request, year, month, day,post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    comments = post.comments.filter(active = True)
+    form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments':comments,
+                  'form': form})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id = post_id,
+                             status = 'published')
+    comment= None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post=post
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                            {'post':post,
+                             'form':form,
+                             'comment': comment})
 # Create your views here.
