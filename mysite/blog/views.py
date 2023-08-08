ed2e4750ce812e92f418c006d04e7d1a65aa87e3
Nik <nik3008@gmail.com>
2023-08-08 20:08:20 +0300

    add similar posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,7 @@ from django.core.mail import send_mail
 from .forms import EmailPostForm, CommentForm
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_share(request, post_id):
@@ -69,11 +70,15 @@ def post_detail(request, year, month, day,post):
                              publish__day=day)
     comments = post.comments.filter(active = True)
     form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments':comments,
-                  'form': form})
+                  'form': form,
+                   'similar_posts':similar_posts})
 
 @require_POST
 def post_comment(request, post_id):
