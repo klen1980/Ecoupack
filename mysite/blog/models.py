2b5aff555fe85b102b951037cc58dd6cf0851760
Nik <nik3008@gmail.com>
2023-08-10 20:09:55 +0300

    add most commented posts yo sidebar

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -20,6 +20,7 @@ class Post (models.Model):
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default = 'draft')
     object = models.Manager()
     published = PublishedManager()
