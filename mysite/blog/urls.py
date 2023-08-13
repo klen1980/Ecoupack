55d4745d083f1772a4ee365e6e312cdf5262986f
Nik <nik3008@gmail.com>
2023-08-13 23:10:36 +0300

    add postgres & search from post

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path ('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name = 'post_comment'),
     path('tag/<slug:tag_slug>/', views.post_list,name='post_list_by_tag'),
-    path('feed/', LatestPostsFeed(), name='post_feed')
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
 
 ]
