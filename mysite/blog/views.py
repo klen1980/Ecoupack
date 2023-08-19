89f1de99e8dabc633b788145fb55c8913e0eb734
Nik <nik3008@gmail.com>
2023-08-19 17:55:30 +0300

    modify search

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,7 +7,7 @@ from .forms import EmailPostForm, CommentForm, SearchForm
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def post_search(request):
@@ -19,9 +19,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A', config='russian') + SearchVector('body', weight='B', config='russian')
+            search_query = SearchQuery(query, config = 'russian')
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-            ).filter(search=query)
+                search=search_vector,
+                rank= SearchRank(search_vector,search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
 
 
     return render(request,
